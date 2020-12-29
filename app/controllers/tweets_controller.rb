00780b48ba609e9ad038170d52b33d2ab32a0fed
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-29 07:53:01 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -34,4 +34,18 @@ class TweetsController < ApplicationController
       redirect to "/login"
     end
   end
+
+  get "/tweets/:id/edit" do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+
+      if @tweet && @tweet.user == current_user
+        erb :"tweets/edit"
+      else
+        redirect to "/tweets"
+      end
+    else
+      redirect to "/login"
+    end
+  end
 end

af36f1c7b83d1e17e8dfb7b993d3cad0e386cd82
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-29 08:02:39 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,12 @@ class UsersController < ApplicationController
     @user = user.find_by_slug(params[:slug])
     erb :"users/show"
   end
+
+  get "/signup" do
+    if !logged_in?
+      erb :"users/create_user", locals: {message: "Please sign up before you sign in"}
+    else
+      redirect to "/tweets"
+    end
+  end
 end

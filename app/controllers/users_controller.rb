02f552c7956ff0944926a16e0d94fd92046e517c
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-29 08:04:58 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,13 @@ class UsersController < ApplicationController
   end
 
   post "/login" do
-    user = User.find_by(username: params[:username])
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    else
+      redirect to "/signup"
+    end
   end
 end

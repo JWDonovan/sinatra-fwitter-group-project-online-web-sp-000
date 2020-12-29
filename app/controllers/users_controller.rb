4df9829136aea04a66cdbf971afdb334bf3fcef0
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-29 08:03:33 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,10 @@ class UsersController < ApplicationController
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
       redirect to "/signup"
     else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to "/tweets"
     end
   end
 end
